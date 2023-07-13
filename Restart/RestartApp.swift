8ecbf04f6e65b30fa0a8a1b4c47a0a1e4b5bc2cb
mhodge11 <micahhodgeutk@icloud.com>
2023-07-13 19:02:40 -0400

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Micah Hodge on 7/13/23.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
